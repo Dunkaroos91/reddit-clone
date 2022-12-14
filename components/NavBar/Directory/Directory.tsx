c09f677bcfeed8203b720476fb1e475a2d4349c0
Coleman Gorham <cmgorham91@gmail.com>
2022-12-14 17:53:34 -0600

    first commit

---
+++ components/NavBar/Directory/Directory.tsx
@@ -0,0 +1,58 @@
+import { ChevronDownIcon } from '@chakra-ui/icons';
+import { Menu, MenuButton, Button, MenuList, MenuItem, Icon, Flex, MenuDivider, Text, Image } from '@chakra-ui/react';
+import { TiHome } from 'react-icons/ti'
+
+import React from 'react';
+import Communities from './Communities';
+import useDirectory from '../../../hooks/useDirectory';
+
+
+
+const UserMenu: React.FC = () => {
+
+    const { directoryState, toggleMenuOpen } = useDirectory()
+    return (
+        <Menu isOpen={directoryState.isOpen}>
+            <MenuButton
+                cursor='pointer'
+                mr={2}
+                ml={{ base: 0, md: 2 }}
+                padding='0px 6px'
+                borderRadius={4}
+                _hover={{ outline: '1px solid', outlineColor: 'gray.200' }}
+                onClick={toggleMenuOpen}
+            >
+                <Flex
+                    alignItems='center'
+                    justify='space-between'
+                    width={{ base: 'auto', lg: '200px' }}>
+                    <Flex
+                        alignItems='center'>
+                        {directoryState.selectedMenuItem.imageURL ? (
+                            <Image src={directoryState.selectedMenuItem.imageURL} borderRadius='full' boxSize='24px' mr={2} />
+                        ) : (
+                            <Icon
+                                fontSize={24}
+                                mr={{ base: 1, md: 2 }}
+                                as={directoryState.selectedMenuItem.icon}
+                                color={directoryState.selectedMenuItem.iconColor}
+                            />
+                        )}
+
+                        <Flex display={{ base: 'none', lg: 'flex' }}>
+                            <Text fontWeight={600} fontSize='10pt'>
+                                {directoryState.selectedMenuItem.displayText}
+                            </Text>
+                        </Flex>
+
+                    </Flex>
+                    <ChevronDownIcon />
+                </Flex>
+            </MenuButton>
+            <MenuList>
+                <Communities />
+            </MenuList>
+        </Menu>
+    )
+}
+export default UserMenu;
---
+++ hooks/useCommunityData.tsx
@@ -0,0 +1,154 @@
+import { collection, doc, getDoc, getDocs, increment, writeBatch } from 'firebase/firestore';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react'
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useRecoilState, useSetRecoilState } from 'recoil';
+import { authModalState } from '../atoms/authModalAtom';
+import { Community, CommunitySnippet, communityState } from '../atoms/communitiesAtom';
+import { auth, firestore } from '../firebase/clientApp';
+
+const useCommunityData = () => {
+    const [user] = useAuthState(auth);
+    const [communityStateValue, setCommunityStateValue] = useRecoilState(communityState)
+    const [loading, setLoading] = useState(false);
+    const setAuthModalState = useSetRecoilState(authModalState)
+    const [error, setError] = useState('')
+    const router = useRouter();
+
+    const onJoinOrLeaveCommunity = (communityData: Community, isJoined: boolean) => {
+        // is the user signed in?
+        // if not => open auth modal
+        if (!user) {
+            setAuthModalState({ open: true, view: 'login' });
+            return;
+        }
+        if (isJoined) {
+            leaveCommunity(communityData.id);
+            return;
+        }
+        joinCommunity(communityData);
+    };
+
+    const getMySnippets = async () => {
+        setLoading(true);
+        try {
+            //get user snippets
+            const snippetDocs = await getDocs(collection(firestore, `users/${user?.uid}/communitySnippets`));
+
+            const snippets = snippetDocs.docs.map(doc => ({ ...doc.data() }))
+            setCommunityStateValue(prev => ({
+                ...prev,
+                mySnippets: snippets as CommunitySnippet[],
+                snippetsFetched: true
+            }))
+            console.log('here are snippets', snippets)
+        } catch (error: any) {
+            console.log('getMySnippets error', error)
+            setError(error.message);
+        }
+        setLoading(false);
+    }
+    const joinCommunity = async (communityData: Community) => {
+        // batch write
+        // creating a new community snippet
+
+        // updating the numberOfMembers
+        try {
+
+            const batch = writeBatch(firestore);
+
+            const newSnippet: CommunitySnippet = {
+                communityId: communityData.id,
+                imageURL: communityData.imageURL || '',
+                isModerator: user?.uid === communityData.creatorId,
+            };
+
+            batch.set(doc(firestore, `users/${user?.uid}/communitySnippets`, communityData.id), newSnippet)
+
+
+            batch.update(doc(firestore, 'communities', communityData.id), {
+                numberOfMembers: increment(1),
+            })
+
+            await batch.commit();
+
+            setCommunityStateValue(prev => ({
+                ...prev,
+                mySnippets: [...prev.mySnippets, newSnippet],
+            }));
+
+
+        } catch (error: any) {
+            console.log('joinCommunity error', error);
+            setError(error.message);
+        }
+        setLoading(false);
+    };
+
+    const leaveCommunity = async (communityId: string) => {
+
+        try {
+            const batch = writeBatch(firestore);
+
+            batch.delete(doc(firestore, `users/${user?.uid}/communitySnippets`, communityId));
+
+            batch.update(doc(firestore, 'communities', communityId), {
+                numberOfMembers: increment(-1),
+            });
+
+            await batch.commit();
+            setCommunityStateValue(prev => ({
+                ...prev,
+                mySnippets: prev.mySnippets.filter(item => item.communityId !== communityId)
+            }))
+        } catch (error: any) {
+            console.log('leaveCommunity error', error.message)
+            setError(error.message);
+        }
+        setLoading(false);
+    };
+
+    const getCommunityData = async (communityId: string) => {
+        try {
+            const communityDocRef = doc(firestore, 'communities', communityId);
+            const communityDoc = await getDoc(communityDocRef);
+
+            setCommunityStateValue((prev) => ({
+                ...prev,
+                currentCommunity: { id: communityDoc.id, ...communityDoc.data() } as Community
+            }))
+        } catch (error) {
+            console.log('getCommunityData', error)
+        }
+    }
+
+    useEffect(() => {
+        if (!user) {
+            setCommunityStateValue((prev) => ({
+                ...prev,
+                mySnippets: [],
+                snippetsFetched: false
+            }));
+            return;
+        };
+        getMySnippets();
+    }, [user]);
+
+    useEffect(() => {
+        const { communityId } = router.query;
+
+        if (communityId && !communityStateValue.currentCommunity) {
+            getCommunityData(communityId as string);
+        }
+    }, [router.query, communityStateValue.currentCommunity])
+
+    return {
+        // data and function
+        communityStateValue,
+        onJoinOrLeaveCommunity,
+        loading,
+    };
+};
+export default useCommunityData;
+
+
